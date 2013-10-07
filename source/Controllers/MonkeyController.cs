efa0cf3b6acd447accae4fb7693fe5a089d8cc52
Gary Thickett <gaz@brainmonkey.co.uk>
2013-10-07 23:09:16 +0100

    initial commit

---
+++ source/Controllers/MonkeyController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Brainmonkey2013.Controllers {
+    public class MonkeyController : Controller {
+
+        public ActionResult Index() {
+            return View();
+        }
+
+    }
+}
